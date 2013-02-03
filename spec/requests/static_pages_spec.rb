d1dab5050ea27566cfb8d00218b5eb9326dfc9d6
Adam Gilgoff <adam.gilg@gmail.com>
2013-02-02 19:55:37 -0800

    Fixed minor bug in static pages rspec test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,8 +22,8 @@ describe "StaticPages" do
       page.should have_selector('h1', text: 'Help')
     end
 
-    it "should have the title" do
-      visit '/static_pages/home'
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
       page.should have_selector('title', 
         text: "Ruby on Rails Tutorial Sample App | Help")
     end
@@ -35,8 +35,8 @@ describe "StaticPages" do
       page.should have_selector('h1', text: 'About Us')
     end
 
-    it "should have the title" do
-      visit '/static_pages/home'
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
       page.should have_selector('title', 
         text: "Ruby on Rails Tutorial Sample App | About Us")
     end
