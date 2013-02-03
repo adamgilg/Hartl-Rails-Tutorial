b0b6f00b2efb335d753bbdc5a290a6b355dfe062
Adam Gilgoff <adam.gilg@gmail.com>
2013-02-02 18:58:21 -0800

    new rails project set up to start the Hartl Rails Tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b6bcb4f7196df18ed20ce625f49354bb7cec6bd09f0a075bb5db8f2bf273a4ba7486362de436ecb59474f36f23e521b9902747e1e4067ba48f5f90d43258288d'
